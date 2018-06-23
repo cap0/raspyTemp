75549d998da1a071765cf54a7e41d075831ca51a
gabriele.gattari <gabriele.gattari@accenture.com>
2018-06-23 08:12:56 +0200

    added some info to page

--- main/src/main/java/gg/GenerateChart.java
+++ main/src/main/java/gg/GenerateChart.java
@@ -5,25 +5,26 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Enumeration;
 import java.util.Properties;
 
 class GenerateChart {
-
-    private static final String BREW_NAME = "brewName";
-    private static final String BREW_DAY_DATE = "brewDayDate";
-
     void generateChart(StatisticalInfo statisticalInfo, String outputFilePath, Properties p) throws Exception {
         Path dashboardTemplateFile = Paths.get(getClass().getClassLoader().getResource("chart_template.html").toURI());
         Charset charset = StandardCharsets.UTF_8;
 
         String dashboardSource = new String(Files.readAllBytes(dashboardTemplateFile), charset);
 
-        String brewName = p.getProperty(BREW_NAME);
-        String brewDate = p.getProperty(BREW_DAY_DATE);
+        Enumeration<String> propertyNames = (Enumeration<String>) p.propertyNames();
+        while (propertyNames.hasMoreElements()) {
+            String propertyName = propertyNames.nextElement();
+            if(propertyName.startsWith("info.")) {
+                dashboardSource = dashboardSource.replaceAll(propertyName, p.getProperty(propertyName));
+            }
+        }
 
-        dashboardSource = dashboardSource.replaceAll(BREW_NAME, brewName);
-        dashboardSource = dashboardSource.replaceAll(BREW_DAY_DATE, brewDate);
         dashboardSource = dashboardSource.replaceAll("XXX", statisticalInfo.temperatures.toString());
+
         Path outputFile = Paths.get(outputFilePath);
 
         Files.write(outputFile, dashboardSource.getBytes(charset));
