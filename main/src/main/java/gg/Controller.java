b41fec849c5df08c15489c03485231452e9f3819
gabriele.gattari <gabriele.gattari@accenture.com>
2019-06-18 20:37:49 +0200

    removed repeated reading of temp value

--- main/src/main/java/gg/Controller.java
+++ main/src/main/java/gg/Controller.java
@@ -4,14 +4,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
 import static gg.Constants.*;
 import static gg.Util.formatTemperature;
 import static gg.Util.toDouble;
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 public class Controller implements Runnable{
 
@@ -42,7 +39,7 @@ public class Controller implements Runnable{
                        Double configDeltaTemp, Double deltaTempWhenCoolingOrWarming, IGPIOController gpioCtrl, LCD lcd, ConnectionChecker connCheck) {
         this.temperatureReader = temperatureReader;
         this.temperatureSettingsPath = temperatureSettingsPath;
-        this.temperatureSettings = new TemperatureSettings(temperatureSettingsPath);
+        this.temperatureSettings = new TemperatureSettings(temperatureSettingsPath); //TODO Move away, it need a restart to work
         this.deltaTempWhenFermenting = configDeltaTemp;
         this.deltaTempWhenCoolingOrWarming = deltaTempWhenCoolingOrWarming;
         this.gpioCtrl = gpioCtrl;
@@ -54,12 +51,6 @@ public class Controller implements Runnable{
 
     private void checkIfTempIsOnRange(LocalDateTime now) {
         logger.debug("checking temperature " + now);
-       /* Optional<Double> wortTempOpt = getWortTemp();
-        if (!wortTempOpt.isPresent()) {
-            return;
-        }
-        Double wortTemp = wortTempOpt.get();
-        */
 
         Double wortTemp = toDouble(temperatureReader.getWorthTemperature());
 
@@ -93,35 +84,6 @@ public class Controller implements Runnable{
 
     private String formatTempRangeForLcd(double lowerBound, double upperBound) {
         return formatTemperature(lowerBound) + "-" + formatTemperature(upperBound) ;
-    }
-
-    private Optional<Double> getWortTemp() {
-        String wortTemperatureValue1 = temperatureReader.getWorthTemperature();
-        sleep1Sec();
-        String wortTemperatureValue2 = temperatureReader.getWorthTemperature();
-        sleep1Sec();
-        String wortTemperatureValue3 = temperatureReader.getWorthTemperature();
-
-        if (isNotBlank(wortTemperatureValue1) && isNotBlank(wortTemperatureValue2) && isNotBlank(wortTemperatureValue3)) {
-            Double t1 = Double.valueOf(wortTemperatureValue1);
-            Double t2 = Double.valueOf(wortTemperatureValue2);
-            Double t3 = Double.valueOf(wortTemperatureValue3);
-            double delta = t1 - t2 + t2 - t3 + t1 - t3;
-            boolean tempIsStable = delta <= 0.1;
-            if (tempIsStable) {
-                return Optional.of(t1);
-            }
-        }
-        logger.warn("temperature not stable: {}, {}, {}", wortTemperatureValue1, wortTemperatureValue2, wortTemperatureValue3);
-        return Optional.empty();
-    }
-
-    private void sleep1Sec() {
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-            logger.error(e.getMessage());
-        }
     }
 
     private void heating() {
